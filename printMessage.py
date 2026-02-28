300e1771a5cc6ed2f28ced3b15e56f75066f0cb8
froelen <marius.trouillon@proton.me>
2026-02-28 20:02:37 +0100

    Add unfinished docstring

--- printMessage.py
+++ printMessage.py
@@ -1,4 +1,5 @@
 def printMessage(message, version):
+    """Prints the given message or the current version based on the 'version' flag."""
     assert isinstance(message, str), "'message' must be a string (type:str)."
     if version:
         print("Current version: alpha 0.1.0")
