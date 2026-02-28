72293a5ce0d5bd25c0e29ee1ba30524a47992777
froelen <marius.trouillon@proton.me>
2026-02-28 20:01:48 +0100

    Add full docstring

--- printMessage.py
+++ printMessage.py
@@ -1,6 +1,12 @@
-def printMessage(message, version):
+def printMessage(message:str, version:bool=False):
+    """
+    Prints 'message'.
+    Args:
+        message: The message to print.
+        version: If True, prints the current version instead of the message.
+    """
     assert isinstance(message, str), "'message' must be a string (type:str)."
     if version:
-        print("Current version: alpha 0.1.0")
+        print("Current version: alpha 0.1.1")
     else:
         print(message)
