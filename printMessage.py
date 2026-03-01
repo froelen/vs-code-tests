7ff4fe3ab7f15c708788fae7ea2e103e2e6937bb
froelen <marius.trouillon@proton.me>
2026-03-01 01:02:42 +0100

    VS Code bug or somrthing

--- printMessage.py
+++ printMessage.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-def printMessage(message, version):
-    """Prints the given message or the current version based on the 'version' flag."""
-=======
 def printMessage(message:str, version:bool=False):
     """
     Prints 'message'.
@@ -9,7 +5,6 @@ def printMessage(message:str, version:bool=False):
         message: The message to print.
         version: If True, prints the current version instead of the message.
     """
->>>>>>> testCodeLens
     assert isinstance(message, str), "'message' must be a string (type:str)."
     if version:
         print("Current version: alpha 0.1.1")
